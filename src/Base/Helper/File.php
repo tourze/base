69d3a0a710c9f78c9af606f9b3a994522951ab53
luzhongpeng <1234qwqw>
2015-06-30 23:29:50 +0800

    add new methods in File helper

--- src/Base/Helper/File.php
+++ src/Base/Helper/File.php
@@ -41,6 +41,40 @@ class File
     }
 
     /**
+     * 创建目录
+     *
+     * @param mixed $dirs
+     * @param int   $mode
+     */
+    public static function mkdir($dirs, $mode = 0777)
+    {
+        self::ensureFileSystem()->mkdir($dirs, $mode);
+    }
+
+    /**
+     * 检测文件是否存在
+     *
+     * @param $files
+     * @return bool
+     */
+    public static function exists($files)
+    {
+        return self::ensureFileSystem()->exists($files);
+    }
+
+    /**
+     * touch创建一个空文件
+     *
+     * @param      $files
+     * @param null $time
+     * @param null $atime
+     */
+    public static function touch($files, $time = null, $atime = null)
+    {
+        self::ensureFileSystem()->touch($files, $time, $atime);
+    }
+
+    /**
      * 删除指定文件
      *
      * @param $files
@@ -48,5 +82,78 @@ class File
     public static function delete($files)
     {
         self::ensureFileSystem()->remove($files);
+    }
+
+    /**
+     * chmod
+     *
+     * @param      $files
+     * @param      $mode
+     * @param int  $umask
+     * @param bool $recursive
+     */
+    public static function chmod($files, $mode, $umask = 0000, $recursive = false)
+    {
+        self::ensureFileSystem()->chmod($files, $mode, $umask, $recursive);
+    }
+
+    /**
+     * @param      $files
+     * @param      $user
+     * @param bool $recursive
+     */
+    public static function chown($files, $user, $recursive = false)
+    {
+        self::ensureFileSystem()->chown($files, $user, $recursive);
+    }
+
+    /**
+     * 重命名指定文件
+     *
+     * @param      $origin
+     * @param      $target
+     * @param bool $overwrite
+     */
+    public static function rename($origin, $target, $overwrite = false)
+    {
+        self::ensureFileSystem()->rename($origin, $target, $overwrite);
+    }
+
+    /**
+     * @param $endPath
+     * @param $startPath
+     * @return string
+     */
+    public static function makePathRelative($endPath, $startPath)
+    {
+        return self::ensureFileSystem()->makePathRelative($endPath, $startPath);
+    }
+
+    /**
+     * 写入文件
+     *
+     * @param string $filename
+     * @param mixed  $content
+     * @param int    $mode
+     */
+    public static function write($filename, $content, $mode = 0666)
+    {
+        self::ensureFileSystem()->dumpFile($filename, $content, $mode);
+    }
+
+    /**
+     * 读取指定文件
+     *
+     * @param string $filename
+     * @return bool|string
+     */
+    public static function read($filename)
+    {
+        if ( ! file_exists($filename))
+        {
+            return false;
+        }
+
+        return file_get_contents($filename);
     }
 }
