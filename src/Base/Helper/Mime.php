2428ebbd3289ba448b4117499c871723bd286563
luzhongpeng <1234qwqw>
2015-07-05 20:41:47 +0800

    更新mime助手类

--- src/Base/Helper/Mime.php
+++ src/Base/Helper/Mime.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace tourze\Base\Helper;
+use tourze\Base\Config;
 
 /**
  * MIME助手类
@@ -10,57 +11,7 @@ namespace tourze\Base\Helper;
 class Mime
 {
 
-    /**
-     * @var array
-     */
-    public static $mimeTypes = [
-        'txt'  => 'text/plain',
-        'htm'  => 'text/html',
-        'html' => 'text/html',
-        'php'  => 'text/html',
-        'css'  => 'text/css',
-        'js'   => 'application/javascript',
-        'json' => 'application/json',
-        'xml'  => 'application/xml',
-        'swf'  => 'application/x-shockwave-flash',
-        'flv'  => 'video/x-flv',
-        // Images
-        'png'  => 'image/png',
-        'jpe'  => 'image/jpeg',
-        'jpeg' => 'image/jpeg',
-        'jpg'  => 'image/jpeg',
-        'gif'  => 'image/gif',
-        'bmp'  => 'image/bmp',
-        'ico'  => 'image/vnd.microsoft.icon',
-        'tiff' => 'image/tiff',
-        'tif'  => 'image/tiff',
-        'svg'  => 'image/svg+xml',
-        'svgz' => 'image/svg+xml',
-        // Archives
-        'zip'  => 'application/zip',
-        'rar'  => 'application/x-rar-compressed',
-        'exe'  => 'application/x-msdownload',
-        'msi'  => 'application/x-msdownload',
-        'cab'  => 'application/vnd.ms-cab-compressed',
-        // Audio/video
-        'mp3'  => 'audio/mpeg',
-        'qt'   => 'video/quicktime',
-        'mov'  => 'video/quicktime',
-        // Adobe
-        'pdf'  => 'application/pdf',
-        'psd'  => 'image/vnd.adobe.photoshop',
-        'ai'   => 'application/postscript',
-        'eps'  => 'application/postscript',
-        'ps'   => 'application/postscript',
-        // MS Office
-        'doc'  => 'application/msword',
-        'rtf'  => 'application/rtf',
-        'xls'  => 'application/vnd.ms-excel',
-        'ppt'  => 'application/vnd.ms-powerpoint',
-        // Open Office
-        'odt'  => 'application/vnd.oasis.opendocument.text',
-        'ods'  => 'application/vnd.oasis.opendocument.spreadsheet',
-    ];
+    public static $configName = 'helper/mime';
 
     /**
      * @param $file
@@ -78,7 +29,9 @@ class Mime
      */
     public static function getExtensionsFromMime($mime)
     {
-        foreach (self::$mimeTypes as $ext => $mimeType)
+        $mimeTypes = Config::load(self::$configName)->asArray();
+
+        foreach ($mimeTypes as $ext => $mimeType)
         {
             if ($mime == $mimeType)
             {
@@ -95,7 +48,9 @@ class Mime
      */
     public static function getMimeFromExtension($ext)
     {
-        return isset(self::$mimeTypes[$ext]) ? self::$mimeTypes[$ext] : 'application/octet-stream';
+        $mimeTypes = Config::load(self::$configName)->asArray();
+
+        return Arr::get($mimeTypes, $ext, 'application/octet-stream');
     }
 
 }
