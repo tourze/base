9c7384481b1c2d7712169594468182a3943d4949
tourze <25803471@qq.com>
2015-08-22 19:35:24 +0800

    修正Url助手类的注释

--- src/Base/Helper/Url.php
+++ src/Base/Helper/Url.php
@@ -21,8 +21,8 @@ class Url
      *
      *     $uri = Request::detectUri();
      *
-     * @return  string  URI
-     * @throws  HelperException
+     * @return string  URI
+     * @throws HelperException
      */
     public static function detectUri()
     {
@@ -32,16 +32,14 @@ class Url
         }
         else
         {
-            // REQUEST_URI and PHP_SELF include the docroot and index
+            // REQUEST_URI和PHP_SELF会包含当前脚本路径
             if (isset($_SERVER['REQUEST_URI']))
             {
                 $uri = $_SERVER['REQUEST_URI'];
                 if ($requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))
                 {
-                    // Valid URL path found, set it.
                     $uri = $requestUri;
                 }
-                // Decode the request URI
                 $uri = rawurldecode($uri);
             }
             elseif (isset($_SERVER['PHP_SELF']))
@@ -57,16 +55,13 @@ class Url
                 throw new HelperException('Unable to detect the URI using PATH_INFO, REQUEST_URI, PHP_SELF or REDIRECT_URL');
             }
 
-            // Get the path from the base URL, including the index file
             $baseUrl = parse_url(Base::$baseUrl, PHP_URL_PATH);
             if (strpos($uri, $baseUrl) === 0)
             {
-                // Remove the base URL from the URI
                 $uri = (string) substr($uri, strlen($baseUrl));
             }
             if (Base::$indexFile && strpos($uri, Base::$indexFile) === 0)
             {
-                // Remove the index file from the URI
                 $uri = (string) substr($uri, strlen(Base::$indexFile));
             }
         }
@@ -75,32 +70,27 @@ class Url
     }
 
     /**
-     * Gets the base URL to the application.
-     * To specify a protocol, provide the protocol as a string or request object.
-     * If a protocol is used, a complete URL will be generated using the
-     * `$_SERVER['HTTP_HOST']` variable.
+     * 获取当前应用的基础路径
      *
-     *     // Absolute URL path with no host or protocol
-     *     echo URL::base();
+     *     // 返回不带主机名和协议的相对路径
+     *     echo Url::base();
      *
-     *     // Absolute URL path with host, https protocol and index.php if set
-     *     echo URL::base('https', true);
+     *     // 带主机名和指定协议的绝对路径
+     *     echo Url::base('https', true);
      *
-     *     // Absolute URL path with host and protocol from $request
-     *     echo URL::base($request);
+     *     // 指定request对象
+     *     echo Url::base($request);
      *
-     * @param   mixed   $protocol Protocol string, [Request], or boolean
-     * @param   boolean $index    Add index file to URL?
-     * @return  string
+     * @param  string|bool|Request $protocol 协议字符串或Request对象或其他
+     * @param  bool                $index    是否在链接中增加缺省文件
+     * @return string
      */
     public static function base($protocol = null, $index = false)
     {
-        // Start with the configured base URL
         $baseUrl = Base::$baseUrl;
 
         if (true === $protocol)
         {
-            // Use the initial request to get the protocol
             $protocol = Request::$initial;
         }
 
@@ -108,7 +98,6 @@ class Url
         {
             if ( ! $protocol->secure)
             {
-                // Use the current protocol
                 list($protocol) = explode('/', strtolower($protocol->protocol));
             }
             else
@@ -119,13 +108,11 @@ class Url
 
         if ( ! $protocol)
         {
-            // Use the configured default protocol
             $protocol = parse_url($baseUrl, PHP_URL_SCHEME);
         }
 
         if (true === $index && ! empty(Base::$indexFile))
         {
-            // Add the index file to the URL
             $baseUrl .= Base::$indexFile . '/';
         }
 
@@ -133,22 +120,20 @@ class Url
         {
             if ($port = parse_url($baseUrl, PHP_URL_PORT))
             {
-                // Found a port, make it usable for the URL
+                // 找到端口
                 $port = ':' . $port;
             }
 
             if ($domain = parse_url($baseUrl, PHP_URL_HOST))
             {
-                // Remove everything but the path from the URL
+                // 移除除了路径外的所有信息
                 $baseUrl = parse_url($baseUrl, PHP_URL_PATH);
             }
             else
             {
-                // Attempt to use HTTP_HOST and fallback to SERVER_NAME
                 $domain = isset($_SERVER['HTTP_HOST']) ? $_SERVER['HTTP_HOST'] : $_SERVER['SERVER_NAME'];
             }
 
-            // Add the protocol and domain to the base URL
             $baseUrl = $protocol . '://' . $domain . $port . $baseUrl;
         }
 
@@ -156,72 +141,61 @@ class Url
     }
 
     /**
-     * Fetches an absolute site URL based on a URI segment.
+     * 返回一个绝对路径URL
      *
-     *     echo URL::site('foo/bar');
+     *     echo Url::site('foo/bar');
      *
-     * @param   string  $uri      Site URI to convert
-     * @param   mixed   $protocol Protocol string or [Request] class to use protocol from
-     * @param   boolean $index    Include the index_page in the URL
-     * @return  string
+     * @param  string $uri      传入URI
+     * @param  mixed  $protocol 协议字符串或[Request]对象
+     * @param  bool   $index    是否包含缺省文件
+     * @return string
      */
-    public static function site($uri = '', $protocol = null, $index = true)
+    public static function site($uri = '', $protocol = null, $index = false)
     {
-        // Chop off possible scheme, host, port, user and pass parts
+        // 关闭可能的计划，主机，端口，用户和其他匹配到的部分
         $path = preg_replace('~^[-a-z0-9+.]++://[^/]++/?~', '', trim($uri, '/'));
 
         if (preg_match('/[^\x00-\x7F]/S', $path))
         {
-            // Encode all non-ASCII characters, as per RFC 1738
+            // 编码所有非ASCII字符，RFC 1738标准
             $path = preg_replace_callback('~([^/]+)~', 'rawurlencode', $path);
         }
 
-        // Concat the URL
         return self::base($protocol, $index) . $path;
     }
 
     /**
-     * Merges the current GET parameters with an array of new or overloaded
-     * parameters and returns the resulting query string.
+     * 返回指定变量的字符串格式
      *
-     *     // Returns "?sort=title&limit=10" combined with any existing GET values
-     *     $query = URL::query(['sort' => 'title', 'limit' => 10]);
+     *     // 返回"?sort=title&limit=10"
+     *     $query = Url::query(['sort' => 'title', 'limit' => 10]);
      *
-     * Typically you would use this when you are sorting query results,
-     * or something similar.
+     * [!!] 参数中带null的话，这个键会被忽略
      *
-     * [!!] Parameters with a null value are left out.
-     *
-     * @param   array   $params Array of GET parameters
-     * @param   boolean $useGet Include current request GET parameters
-     * @return  string
+     * @param  array $params 参数列表Array of GET parameters
+     * @param  bool  $useGet 是否合并当前的$_GET数组
+     * @return string
      */
-    public static function query(array $params = null, $useGet = true)
+    public static function query(array $params = null, $useGet = false)
     {
         if ($useGet)
         {
             if (null === $params)
             {
-                // Use only the current parameters
                 $params = $_GET;
             }
             else
             {
-                // Merge the current and new parameters
                 $params = Arr::merge($_GET, $params);
             }
         }
 
         if (empty($params))
         {
-            // No query parameters
             return '';
         }
 
-        // Note: http_build_query returns an empty string for a params array with only null values
         $query = http_build_query($params, '', '&');
-
-        // Don't prepend '?' to an empty string
         return ($query === '') ? '' : ('?' . $query);
     }
 }
