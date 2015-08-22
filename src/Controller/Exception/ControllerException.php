5e6cf472b106294e8b8f3d8fbd328504b2353134
tourze <25803471@qq.com>
2015-08-22 20:53:31 +0800

    增加控制器异常类的注释

--- src/Controller/Exception/ControllerException.php
+++ src/Controller/Exception/ControllerException.php
@@ -4,6 +4,11 @@ namespace tourze\Controller\Exception;
 
 use tourze\Base\Exception\BaseException;
 
+/**
+ * 控制器异常类
+ *
+ * @package tourze\Controller\Exception
+ */
 class ControllerException extends BaseException
 {
 }
--- src/Controller/Exception/JsonpInvalidParameterException.php
+++ src/Controller/Exception/JsonpInvalidParameterException.php
@@ -2,6 +2,11 @@
 
 namespace tourze\Controller\Exception;
 
+/**
+ * JSONP中缺失callback参数会抛出这个异常
+ *
+ * @package tourze\Controller\Exception
+ */
 class JsonpInvalidParameterException extends ControllerException
 {
 }
