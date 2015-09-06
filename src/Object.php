78f817b0d19de306423f24b2371f281548861bd5
tourze <dev@tourze.com>
2015-09-06 21:18:39 +0800

    更新object基础类

--- src/Object.php
+++ src/Object.php
@@ -69,7 +69,7 @@ class Object
     }
 
     /**
-     * 返回当前类的类名，关键词：__延时加载__
+     * 返回当前类的类名，关键词：延时加载
      *
      * @static
      * @access public
@@ -138,7 +138,6 @@ class Object
     /**
      * 返回对象属性，实现getter功能
      *
-     * @access public
      * @param  string $name 属性名
      * @return mixed 指定的属性值
      * @throws UnknownPropertyException 属性值不存在的话
@@ -146,21 +145,23 @@ class Object
      */
     public function __get($name)
     {
+        $camelizeName = $this->__buildCamelizeName($name);
+
         // 默认的getter方法
-        $method = 'get' . $this->__buildCamelizeName($name);
+        $method = 'get' . $camelizeName;
         if (method_exists($this, $method))
         {
             return $this->$method();
         }
 
         // 有可能只需要返回布尔值
-        $method = 'is' . $this->__buildCamelizeName($name);
+        $method = 'is' . $camelizeName;
         if (method_exists($this, $method))
         {
             return (bool) $this->$method();
         }
 
-        $method = 'set' . $this->__buildCamelizeName($name);
+        $method = 'set' . $camelizeName;
         if (method_exists($this, $method))
         {
             throw new InvalidCallException('Getting write-only property: {class}::{name}', [
@@ -189,14 +190,16 @@ class Object
      */
     public function __set($name, $value)
     {
-        $method = 'set' . $this->__buildCamelizeName($name);
+        $camelizeName = $this->__buildCamelizeName($name);
+
+        $method = 'set' . $camelizeName;
         if (method_exists($this, $method))
         {
             $this->$method($value);
             return;
         }
 
-        $method = 'get' . $this->__buildCamelizeName($name);
+        $method = 'get' . $camelizeName;
         if (method_exists($this, $method))
         {
             throw new InvalidCallException('Setting read-only property: ' . get_class($this) . '::' . $name);
@@ -215,7 +218,9 @@ class Object
      */
     public function __isset($name)
     {
-        $getter = 'get' . ucfirst($name);
+        $camelizeName = $this->__buildCamelizeName($name);
+
+        $getter = 'get' . $camelizeName;
         if (method_exists($this, $getter))
         {
             return null !== $this->$getter();
@@ -234,86 +239,16 @@ class Object
      */
     public function __unset($name)
     {
-        $setter = 'set' . ucfirst($name);
+        $camelizeName = $this->__buildCamelizeName($name);
+
+        $setter = 'set' . $camelizeName;
         if (method_exists($this, $setter))
         {
             $this->$setter(null);
         }
-        elseif (method_exists($this, 'get' . ucfirst($name)))
+        elseif (method_exists($this, 'get' . $camelizeName))
         {
             throw new InvalidCallException('Deleting read-only property: ' . get_class($this) . '::' . $name);
-        }
-    }
-
-    /**
-     * 将指定的属性转为布尔值
-     *
-     * @param $property
-     * @param $value
-     * @throws BaseException
-     */
-    public function toBoolean($property, $value)
-    {
-        $trueValues = [true, 1, '1', 'true', 'yes', 'on'];
-        $falseValues = [false, 0, '0', 'false', 'no', 'off', ''];
-
-        if (is_string($value))
-        {
-            $value = strtolower($value);
-        }
-
-        $result = null;
-        if ($result === null && in_array($value, $trueValues, true))
-        {
-            $result = true;
-        }
-        if ($result === null && in_array($value, $falseValues, true))
-        {
-            $result = false;
-        }
-
-        // 强制转换
-        try
-        {
-            $this->$property = (bool) $result;
-        }
-        catch (Exception $ex)
-        {
-            throw new BaseException('Property :property is not existed or the value is cast.', [
-                ':property' => $property,
-            ]);
-        }
-    }
-
-    /**
-     * 转换指定字段数据为整形
-     *
-     * @param $property
-     * @param $value
-     * @throws BaseException
-     */
-    public function toInteger($property, $value)
-    {
-        $result = 0;
-
-        if (is_integer($value))
-        {
-            $result = $value;
-        }
-        if (is_numeric($value) && ($value == (integer) $value))
-        {
-            $result = (integer) $value;
-        }
-
-        try
-        {
-            $this->$property = (int) $result;
-        }
-        catch (Exception $e)
-        {
-            throw new BaseException('Property :property is not existed or the value is cast.', [
-                ':property' => $property,
-            ]);
         }
     }
 
@@ -323,8 +258,6 @@ class Object
      * @param  string $name      属性名
      * @param  bool   $checkVars 是否检查成员变量
      * @return bool
-     * @see canGetProperty()
-     * @see canSetProperty()
      */
     public function hasProperty($name, $checkVars = true)
     {
@@ -337,11 +270,11 @@ class Object
      * @param  string $name      属性名
      * @param  bool   $checkVars 是否检查成员变量
      * @return bool
-     * @see canSetProperty()
      */
     public function canGetProperty($name, $checkVars = true)
     {
-        return method_exists($this, 'get' . ucfirst($name)) || $checkVars && property_exists($this, $name);
+        $camelizeName = $this->__buildCamelizeName($name);
+        return method_exists($this, 'get' . $camelizeName) || $checkVars && property_exists($this, $name);
     }
 
     /**
@@ -350,11 +283,11 @@ class Object
      * @param string $name      属性名
      * @param bool   $checkVars 是否检查成员变量
      * @return bool
-     * @see canGetProperty()
      */
     public function canSetProperty($name, $checkVars = true)
     {
-        return method_exists($this, 'set' . ucfirst($name)) || $checkVars && property_exists($this, $name);
+        $camelizeName = $this->__buildCamelizeName($name);
+        return method_exists($this, 'set' . $camelizeName) || $checkVars && property_exists($this, $name);
     }
 
     /**
