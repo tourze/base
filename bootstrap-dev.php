d1ac47d185f9e3c08e0e757497669b6c6e37ec31
tourze <dev@tourze.com>
2015-09-14 23:50:46 +0800

    删除SAE的判断

--- bootstrap-dev.php
+++ bootstrap-dev.php
@@ -9,12 +9,6 @@ if (is_file(__DIR__ . '/vendor/autoload.php'))
     require __DIR__ . '/vendor/autoload.php';
 }
 
-// 检测是否在SAE中
-if ( ! defined('IN_SAE'))
-{
-    define('IN_SAE', function_exists('sae_debug'));
-}
-
 // 指定配置加载目录
 Config::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
 
